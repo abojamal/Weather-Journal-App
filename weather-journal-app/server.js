e8d9a1db046568aa39c6017fc22bc3969f1fc2cb
shafik <shafiksharif@gmail.com>
2020-09-22 14:27:19 +0300

    docs:version1

--- weather-journal-app/server.js
+++ weather-journal-app/server.js
@@ -2,18 +2,36 @@
 projectData = {};
 
 // Require Express to run server and routes
-
+const express = require('express');
 // Start up an instance of app
-
+const app = express();
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
+// get data route
+app.get('/all', (req, res) => {
+  res.send(projectData);
+});
+
+// POST route
+app.post('/add', (req, res) => {
+  projectData.temperature = req.body.temperature;
+  projectData.date = req.body.date;
+  projectData.content = req.body.content;
+  res.send(projectData);
+});
--- weather-journal-app/website/app.js
+++ weather-journal-app/website/app.js
@@ -1,5 +1,74 @@
 /* Global Variables */
-
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?';
+const apiKey = '&appid=dc73540107e240a2b38254747461f5f0';
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+//add 1 to month as it starts with zero as January
+let month = d.getMonth() + 1;
+let newDate = month + '.' + d.getDate() + '.' + d.getFullYear();
+//get api and user feelings then post data to endpoint then update UI
+let generateData = () => {
+  const content = document.getElementById('feelings').value;
+  const zipcode = document.getElementById('zip').value;
+  //get weather API
+  retrieveData(baseURL, zipcode, apiKey).then((data) => {
+    console.log(data);
+    // post  API and user entered content to our endpoint object "projectData"
+    postData('/add', {
+      temperature: data.main.temp,
+      date: newDate,
+      content: content,
+    });
+    //updates UI
+    updateUI();
+  });
+};
+
+// Async POST
+const postData = async (url = '', data = {}) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    const newData = await response.json();
+    return newData;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+// Async GET
+const retrieveData = async (baseURL, zipcode, apiKey) => {
+  const request = await fetch(
+    `${baseURL}zip=${zipcode},us${apiKey}&units=metric`
+  );
+  try {
+    const data = await request.json();
+    return data;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+//update UI
+const updateUI = async () => {
+  const request = await fetch('/all');
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    document.getElementById('date').innerHTML = allData.temperature;
+    document.getElementById('temp').innerHTML = allData.date;
+    document.getElementById('content').innerHTML = allData.content;
+  } catch (error) {
+    console.log('error', error);
+  }
+};
+
+//click event
+document.getElementById('generate').addEventListener('click', generateData);
